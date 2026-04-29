b935afb53022230c2f70c9ce7abf2ed207424112
fida fathima ns <fidafathimans1@gmail.com>
2026-04-29 18:11:17 +0530

    cleaned up folder and kept only 7 files

--- DOM/change-text dynamically/change-text.js
+++
@@ -1,6 +0,0 @@
-const messageHeading = document.getElementById('message');
-const actionButton = document.getElementById('changeBtn');
-actionButton.addEventListener('click', function() {
-    
-    messageHeading.textContent = "WELCOME";
-});
